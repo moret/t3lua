3d820874409f45c83be54f0ae6959f5267e7d502
manhente.thiago <manhente.thiago@26d0c0be-f7e5-07be-c1a4-1f17202a1d72>
2010-07-04 23:45:10 +0000

    t3daemonscode.lua - Correção para trabalhar com vários grupos (mudança de groups.group para groups[group]). - Adição da função getMembers() para retornar a tabela de membros de um grupo.
    t3utils.lua
    - Adição da função table.copy(), que copia o conteúdo de uma tabela para outra (diferente de tab1 = tab2, que só copia a referência).
    
    Primeira versão do mutex.
    - Ao ter pedido de mutex negado, aborta (chama cb_err)
    - Não possui criação de mutex (tal como dalua.mutex.create()).
    - Não resistente á falhas de computadores de que se espera autorização para o mutex (bloqueia o pedinte).
    
    Possíveis melhorias no mutex:
    - Não abortar ao ter pedido de mutex negado (manter lista de espera)
    - Evitar bloqueio em casos de falha de computadores de que se espera autorização para o mutex (talvez com um timer)

--- trunk/src/t3daemonscode.lua
+++ trunk/src/t3daemonscode.lua
@@ -5,6 +5,32 @@ sequencer = nil
 seq = 1
 groups = {}
 
+
+--[[
+| Get the ids of all members of the group.
+| Table with members is returned as a string argument to the callback funciton.
+|
+| @param group Name of the group. [string]
+| @param src ID (as alua.id) of the caller process.
+| @param cb Name of the caller's callback to handle the returned values. [string]
+|			Must have the following interface: cb(group, table_as_string[, extra_args])
+| @param extra_args Extra arguments to be passed to the callback function. [string]
+--]]
+function getMembers(group, src, cb, extra_args)
+	local table_as_string = table.tostring(groups[group] or {})
+	
+	local msg
+	if extra_args then
+		msg = cb .. "('" .. group .. "', " ..
+		             "'" .. table_as_string .. "', " ..
+		             "'" .. extra_args .. "')"
+	else
+		msg =  cb .. "('" .. group .. "', '" .. table_as_string .. "')"
+	end
+	
+	alua.send(src, msg)
+end
+
 function join(group, src)
 	for _, daemon in pairs(alua.getdaemons()) do
 		alua.send(daemon, "__join(\"" .. group .. "\", \"" .. src .. "\")")
@@ -12,16 +38,16 @@ function join(group, src)
 end
 
 function __join(group, src)
-	if not groups.group then
-		groups.group = {}
+	if not groups[group] then
+		groups[group] = {}
 	end
-	groups.group[src] = true
+	groups[group][src] = true
 	print("joined " .. src .. " to group " .. group .. " on daemon " .. alua.daemonid)
 end
 
 function send(group, src, data)
-	if groups.group then
-		for dst in pairs(groups.group) do
+	if groups[group] then
+		for dst in pairs(groups[group]) do
 			alua.send_event(dst, t3lua.events.listen, {data = data, group = group, src = src})
 		end
 	end
@@ -47,8 +73,8 @@ function leave(group, src)
 end
 
 function __leave(group, src)
-	if groups.group then
-		groups.group[src] = nil
+	if groups[group] then
+		groups[group][src] = nil
 		print("left " .. src .. " from group " .. group .. " on daemon " .. alua.daemonid)
 	end
 end
