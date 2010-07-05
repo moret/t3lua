32bee13502ef42ece6eaa8aa45fdf912d683c38b
manhente.thiago <manhente.thiago@26d0c0be-f7e5-07be-c1a4-1f17202a1d72>
2010-07-05 14:07:51 +0000

    t3lua-mutex.lua - Adição da função t3lua.mutex.wrap() para encapsular chamadas de funções com argumentos em uma função temporária sem argumentos para uso com t3lua.mutex.enter().
    testmutex.lua
    - Aprimoramento no teste para receber mais parâmetros por linha de comando.
    - Aprimoramento no teste para usar t3lua.mutex.wrap().

--- src/testmutex.lua
+++ src/testmutex.lua
@@ -5,13 +5,20 @@ require "socket"
 
 group = arg[1] or "grupoA"
 mutex = arg[2] or "mutexI"
+sleep = arg[3] or 15
+msg   = arg[4] or "Hello!"
 
 
-local function crcb()
+local function crcb(time, message)
 	print("Entered critical region.")
-	socket.select(nil, nil, 15)
+	
+	print("Waiting for:", time)
+	socket.select(nil, nil, time)
+	print("Woke up and said:", message)
 	t3lua.mutex.leave(group, mutex)
+	
 	print("Left critical region.")
+	
 	t3lua.leave(group)
 	alua.quit()
 end
@@ -27,6 +34,8 @@ end
 local function conncb()
 	print("alua.id", alua.id, "alua.daemonid", alua.daemonid)
 	print("Entering critical region...")
+	
+	local crcb = t3lua.mutex.wrap(crcb, sleep, msg)
 	t3lua.mutex.enter(group, mutex, crcb, crerrcb)
 end
 
